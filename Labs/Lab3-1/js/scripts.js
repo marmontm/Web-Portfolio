f88e8fcca4a1085215372fae6b26d731531f4780
Maxime MARMONT <marmontm@users.noreply.github.com>
2018-01-30 10:35:10 +0200

    Lab3.1: Add press Enter to send message

--- Labs/Lab3-1/js/scripts.js
+++ Labs/Lab3-1/js/scripts.js
@@ -25,3 +25,29 @@ function sendText() {
 function sendEmoji() {
     connection.send("LOGO");
 }
+
+
+
+
+/*
+* Simulate a click on 'Send' button
+* when pressing Enter.
+*
+* Script from w3schools.com
+* 'https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp'
+*
+*/
+
+
+// Get the input field
+var input = document.getElementById("input-textMessage");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+        // Trigger the button element with a click
+        document.getElementById("btn-send").click();
+    }
