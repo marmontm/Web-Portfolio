eab112106fa4a554f67716a2f1e8b0a91a47c7d4
Maxime MARMONT <marmontm@users.noreply.github.com>
2018-02-02 15:49:43 +0200

    Lab3.3: Volume control added

--- Labs/Lab3-3/js/scripts.js
+++ Labs/Lab3-3/js/scripts.js
@@ -1,6 +1,8 @@
+var audioStream = document.getElementById("audio-stream");
 var btnPlay = document.getElementById("btn-play");
 var btnPause = document.getElementById("btn-pause");
-var audioStream = document.getElementById("audio-stream");
+var sldVolume = document.getElementById("sld-volume");
+
 
 btnPlay.onclick = function (ev) {
     audioStream.play();
@@ -8,4 +10,16 @@ btnPlay.onclick = function (ev) {
 
 btnPause.onclick = function (ev) {
     audioStream.pause();
+}
+
+audioStream.onvolumechange = function (ev) {
+    sldVolume.value = audioStream.volume * 100;
+}
+
+sldVolume.oninput = function (ev) {
+    audioStream.volume = sldVolume.value / 100;
+}
+
+window.onload = function () {
+    audioStream.volume = .8;
 }
