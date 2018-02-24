4eceef21e5023b874e77e9acbbee7316d22b7b63
Maxime MARMONT <marmontm@users.noreply.github.com>
2018-02-24 23:56:53 +0200

    Apply new style with Lab1 page

---
+++ portfolio/labs/Lab4-2/js/scripts.js
@@ -0,0 +1,55 @@
+var myCanvas = document.getElementById("drawing");
+var myContext = myCanvas.getContext("2d");
+var lblScore = document.getElementById("score");
+var lblRemainingTime = document.getElementById("remaining-time");
+var myGame = null;
+
+function Target(posX, posY, sizeX, sizeY) {
+    this.posX = posX;
+    this.posY = posY;
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+}
+
+function Game(timeSec) {
+    this.remainingTime = timeSec;
+    this.myTimer = null;
+    this.isPlaying = false;
+
+    this.play = function () {
+        lblRemainingTime.innerHTML = this.remainingTime;
+        this.myTimer = setInterval(this.tick, 1000);
+        this.isPlaying = true;
+    };
+
+    this.stop = function () {
+        clearInterval(this.myTimer);
+        this.isPlaying = false;
+    };
+
+    this.tick = function () {
+        this.remainingTime--;
+        lblRemainingTime.innerHTML = this.remainingTime;
+        if (this.remainingTime <= 0) {
+            this.stop();
+        }
+    };
+
+    this.reset = function () {
+        if (this.isPlaying){
+            this.stop();
+        }
+        this.remainingTime = 10;
+    }
+}
+
+myCanvas.addEventListener("click", function () {
+    if (myGame != "") {
+        if (myGame.isPlaying){
+            // check if cursor is on target
+        }
+        else {
+
+        }
+    }
+};
