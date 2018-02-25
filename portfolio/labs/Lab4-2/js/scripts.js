3199ba428f04a33bbde1bdcf948d8b1384e19a47
Maxime MARMONT <marmontm@users.noreply.github.com>
2018-02-26 00:25:01 +0200

    Update3: Lab4 - Canvas game

--- portfolio/labs/Lab4-2/js/scripts.js
+++ portfolio/labs/Lab4-2/js/scripts.js
@@ -1,80 +1,105 @@
+/* UI elements*/
+
 var myCanvas = document.getElementById("drawing");
 var myContext = myCanvas.getContext("2d");
 var lblScore = document.getElementById("score");
 var lblRemainingTime = document.getElementById("remaining-time");
-var myGame = null;
-var myTarget = null;
+var lblMsg = document.getElementById("action");
+
+
+/* Game settings and objects */
+
+var score = 0;
+var remainingtime = 0;
+var msg = null;
+var isPlaying = false;
+
+var myTimer = null;
+var myTarget = {
+    posX: 0,
+    posY: 0,
+    sizeX: 50,
+    sizeY: 50
+}
 var positionCursor = {
     x: 0,
     y: 0
 };
 
-function Target(posX, posY, sizeX, sizeY) {
-    this.posX = posX;
-    this.posY = posY;
-    this.sizeX = sizeX;
-    this.sizeY = sizeY;
-}
 
-function Game(timeSec) {
-    this.remainingTime = timeSec;
-    this.score = 0;
-    this.myTimer = null;
-    this.isPlaying = false;
-
-    this.play = function () {
-        lblRemainingTime.innerHTML = this.remainingTime;
-        this.myTimer = setInterval(this.tick, 1000);
-        this.isPlaying = true;
-    };
-
-    this.stop = function () {
-        clearInterval(this.myTimer);
-        this.isPlaying = false;
-    };
-
-    this.tick = function () {
-        this.remainingTime--;
-        lblRemainingTime.innerHTML = this.remainingTime;
-        if (this.remainingTime <= 0) {
-            this.stop();
-        }
-    };
-
-    this.reset = function () {
-        if (this.isPlaying){
-            this.stop();
-        }
-        this.remainingTime = 10;
-    };
-
-    this.increaseScore = function (){
-        this.score++;
-    }
-}
+/* Event listeners*/
 
 myCanvas.addEventListener("click", function (ev) {
-    if (myGame != null) {
-        if (myGame.isPlaying){
-            positionCursor = getCursorPos(ev);
-            if(
-                ((myTarget.posX <= positionCursor.x)&&(positionCursor <= (myTarget.posX + myTarget.sizeX)))
-                && ((myTarget.posY <= positionCursor.y)&&(positionCursor <= (myTarget.posY + myTarget.sizeY)))
-            ){
-                myGame.increaseScore();
-                clearContext();
-            }
+    if (!isPlaying) {
+        startGame();
+    }
+    else{
+        positionCursor = getCursorPos(ev);
+        var isOnAxisX = ( (myTarget.posX <= positionCursor.x) && (positionCursor.x <= (myTarget.posX + myTarget.sizeX)) );
+        var isOnAxisY = ( (myTarget.posY <= positionCursor.y) && (positionCursor.y <= (myTarget.posY + myTarget.sizeY)) );
+        if (isOnAxisX && isOnAxisY) {
+            console.log("Hit target: success");
+            msg = "Yeeaah! Another one!";
+            score++;
+            displayRandomTarget();
         }
         else {
-            myGame = new Game(10);
-            myGame.play();
+            console.log("Hit target: fail");
+            msg = "Fail! Try again!";
         }
     }
-    else {
-        myGame = new Game(10);
-        myGame.play();
-    }
+    updateUI();
 });
+
+
+/* Game management */
+
+function updateUI() {
+    lblScore.innerHTML = score;
+    lblRemainingTime.innerHTML = remainingtime;
+    lblMsg.innerHTML = msg;
+}
+
+function startGame(){
+    score = 0;
+    remainingtime = 10;
+    msg = "Hit as many targets as possible! Gooooo !";
+    myCanvas.style.backgroundColor = "#ededed";
+
+    displayRandomTarget();
+
+    myTimer = setInterval(tick, 1000); // Call fct tick() each 1000ms
+    isPlaying = true;
+    updateUI();
+
+    console.log("Game started");
+}
+
+function stopGame(){
+    msg = "Game over! Click the canvas to start another game...";
+    myCanvas.style.backgroundColor = "lightgrey";
+
+    clearInterval(myTimer);
+    isPlaying = false;
+    clearContext();
+    updateUI();
+
+    console.log("Game stopped");
+}
+
+function tick() {
+    remainingtime--;
+    if (remainingtime <= 0) {
+        stopGame();
+    }
+    updateUI();
+}
+
+/* Basic functions*/
+
+function getRandom(min, max) {
+    return Math.floor((Math.random() * max) + min);
+}
 
 
 /* Returns current position of cursor in canvas */
@@ -88,4 +113,23 @@ function getCursorPos(event){
 
 function clearContext() {
     myContext.clearRect(0, 0, myCanvas.width, myCanvas.height);
+}
+
+function displayRandomTarget() {
+    clearContext();
+    var maxX = myCanvas.width - myTarget.sizeX;
+    var maxY = myCanvas.height - myTarget.sizeY;
+    myTarget.posX = getRandom(1, maxX);
+    myTarget.posY = getRandom(1, maxY);
+    console.log("New target:");
+    console.log(myTarget);
+    drawTarget(myTarget);
+}
+
+function drawTarget(target) {
+    myContext.beginPath();
+    myContext.fillStyle = 'rgb(247, 79, 79)';
+    myContext.fillRect(target.posX, target.posY, target.sizeX, target.sizeY);
+    myContext.stroke();
+    myContext.closePath();
 }
